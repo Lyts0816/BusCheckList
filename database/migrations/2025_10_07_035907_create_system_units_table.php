83c55a5dac4060d5b6bff637c80b11a6746509c5
Macrhys <mackmondejar@gmail.com>
2025-10-07 13:03:49 +0800

    create system units

---
+++ database/migrations/2025_10_07_035907_create_system_units_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('system_units', function (Blueprint $table) {
+            $table->id();
+            $table->string('asset_code', 50);
+            $table->string('serial_number', 50)->unique();
+            $table->string('model', 20);
+            $table->date('date_aquired');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('system_units');
+    }
+};

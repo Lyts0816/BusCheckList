79a5e109a0fc522dbe237ee83d652c30e6103bcc
Macrhys <mackmondejar@gmail.com>
2025-10-12 16:51:19 +0800

    remove dbNotif

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,7 +42,7 @@ class AdminPanelProvider extends PanelProvider
 
             ])
             ->login()
-            ->databaseNotifications()
+            // ->databaseNotifications()
             ->colors([
                 'primary' => Color::Yellow,
             ])

7de57bce141fbf81a1a9a71f7bc1ef8e0de0dd13
Macrhys <mackmondejar@gmail.com>
2026-01-30 08:02:23 +0800

    remove testing codes

--- app/Filament/Resources/DispatchedTrips/Pages/CreateDispatchedTrips.php
+++ app/Filament/Resources/DispatchedTrips/Pages/CreateDispatchedTrips.php
@@ -8,20 +8,4 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDispatchedTrips extends CreateRecord
 {
     protected static string $resource = DispatchedTripsResource::class;
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        // Convert hours + minutes to total_travel_time_minutes
-        $data['total_travel_time_minutes'] = 
-            (($data['hours'] ?? 0) * 60) + ($data['minutes'] ?? 0);
-        
-        // Convert add_time_hours + add_time_minutes to total_add_time_minutes
-        $data['total_add_time_minutes'] = 
-            (($data['add_time_hours'] ?? 0) * 60) + ($data['add_time_minutes'] ?? 0);
-        
-        // Remove temporary fields
-        unset($data['hours'], $data['minutes'], $data['add_time_hours'], $data['add_time_minutes']);
-        
-        return $data;
-    }
 }
--- app/Filament/Resources/DispatchedTrips/Pages/EditDispatchedTrips.php
+++ app/Filament/Resources/DispatchedTrips/Pages/EditDispatchedTrips.php
@@ -19,35 +19,4 @@ class EditDispatchedTrips extends EditRecord
         ];
     }
 
-        protected function mutateFormDataBeforeFill(array $data): array
-        {
-            // Convert total_travel_time_minutes to hours and minutes
-            $travelTotalMinutes = $data['total_travel_time_minutes'] ?? 0;
-            $data['hours'] = intdiv($travelTotalMinutes, 60);
-            $data['minutes'] = $travelTotalMinutes % 60;
-            
-            // Convert total_add_time_minutes to add_time_hours and add_time_minutes
-            $addTimeTotalMinutes = $data['total_add_time_minutes'] ?? 0;
-            $data['add_time_hours'] = intdiv($addTimeTotalMinutes, 60);
-            $data['add_time_minutes'] = $addTimeTotalMinutes % 60;
-            
-            return $data;
-        }
-
-        protected function mutateFormDataBeforeSave(array $data): array
-        {
-            // Convert hours + minutes back to total_travel_time_minutes
-            $data['total_travel_time_minutes'] = 
-                (($data['hours'] ?? 0) * 60) + ($data['minutes'] ?? 0);
-            
-            // Convert add_time_hours + add_time_minutes back to total_add_time_minutes
-            $data['total_add_time_minutes'] = 
-                (($data['add_time_hours'] ?? 0) * 60) + ($data['add_time_minutes'] ?? 0);
-            
-            // Remove temporary form fields
-            unset($data['hours'], $data['minutes'], $data['add_time_hours'], $data['add_time_minutes']);
-            
-            return $data;
-        }
-
 }
